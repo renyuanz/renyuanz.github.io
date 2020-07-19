5ada79b6ff668db4cfc9f7a17320bb01a6bd66e7
ry.zou <ry.zou@aftership.com>
2020-07-19 22:51:26 +0800

    🐛 (toggle): fix toggle default value bug

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,10 @@
 import React from "react"
 import { Link } from "gatsby"
 import { ThemeToggler } from "gatsby-plugin-dark-mode"
-import Toggle from "./Toggle"
+import Toggle from "react-toggle"
 import { rhythm, scale } from "../utils/typography"
 import "./global.css"
+import "./Toggle.css"
 import sun from "../assets/sun.png"
 import moon from "../assets/moon.png"
 
@@ -57,7 +58,6 @@ const Layout = ({ location, title, children }) => {
       style={{
         backgroundColor: "var(--bg)",
         color: "var(--textNormal)",
-        transition: "color 0.2s ease-out, background 0.2s ease-out",
       }}
     >
       <div
@@ -83,12 +83,11 @@ const Layout = ({ location, title, children }) => {
 
               return (
                 <Toggle
+                  defaultChecked={isDarkMode}
                   icons={{
                     checked: (
                       <img
                         src={moon}
-                        width="16"
-                        height="16"
                         role="presentation"
                         style={{ pointerEvents: "none" }}
                       />
@@ -96,14 +95,11 @@ const Layout = ({ location, title, children }) => {
                     unchecked: (
                       <img
                         src={sun}
-                        width="16"
-                        height="16"
                         role="presentation"
                         style={{ pointerEvents: "none" }}
                       />
                     ),
                   }}
-                  checked={isDarkMode}
                   onChange={e => toggleTheme(isDarkMode ? "light" : "dark")}
                 />
               )
