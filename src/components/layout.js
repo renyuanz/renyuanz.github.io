397f52c2a6f1908b7e3d83bcba70c6d3af9469f8
ry.zou <ry.zou@aftership.com>
2020-07-19 19:27:20 +0800

    ✨ (polish): implement dark mode

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,10 @@
 import React from "react"
 import { Link } from "gatsby"
+import { ThemeToggler } from "gatsby-plugin-dark-mode"
+import DarkModeToggle from "react-dark-mode-toggle"
 
 import { rhythm, scale } from "../utils/typography"
+import "./global.css"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
@@ -12,7 +15,7 @@ const Layout = ({ location, title, children }) => {
       <h1
         style={{
           ...scale(1.5),
-          marginBottom: rhythm(1.5),
+          marginBottom: 0,
           marginTop: 0,
         }}
       >
@@ -33,6 +36,7 @@ const Layout = ({ location, title, children }) => {
         style={{
           fontFamily: `Montserrat, sans-serif`,
           marginTop: 0,
+          marginBottom: 0,
         }}
       >
         <Link
@@ -50,19 +54,50 @@ const Layout = ({ location, title, children }) => {
   return (
     <div
       style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
-        maxWidth: rhythm(24),
-        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        backgroundColor: "var(--bg)",
+        color: "var(--textNormal)",
+        transition: "color 0.2s ease-out, background 0.2s ease-out",
       }}
     >
-      <header>{header}</header>
-      <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
-      </footer>
+      <div
+        style={{
+          marginLeft: `auto`,
+          marginRight: `auto`,
+          maxWidth: rhythm(24),
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        }}
+      >
+        <header
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginBottom: rhythm(1.5),
+          }}
+        >
+          {header}
+          <ThemeToggler>
+            {({ toggleTheme, theme }) => {
+              const isDarkMode = theme === "dark"
+
+              return (
+                <DarkModeToggle
+                  onChange={e => toggleTheme(isDarkMode ? "light" : "dark")}
+                  checked={isDarkMode}
+                  size={60}
+                />
+              )
+            }}
+          </ThemeToggler>
+        </header>
+
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+      </div>
     </div>
   )
 }
